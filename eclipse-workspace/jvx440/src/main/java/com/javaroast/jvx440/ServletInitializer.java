87849582db10337f005cf426147738b8b5f720d9
박민석 <141478303+pakms980319@users.noreply.github.com>
2024-01-28 17:46:34 +0900

    Add files via upload

---
+++ eclipse-workspace/jvx440/src/main/java/com/javaroast/jvx440/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.javaroast.jvx440;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jvx440Application.class);
+	}
+
+}
---
+++ eclipse-workspace/jvx440/src/test/java/com/javaroast/jvx440/Jvx440ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaroast.jvx440;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jvx440ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
